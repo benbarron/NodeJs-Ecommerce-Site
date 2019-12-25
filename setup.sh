7f922d731a65fcb0c271459e4eaf92ccd82f541e
Ben Barron <bjbarron@uark.edu>
2019-12-24 23:01:10 -0600

    commit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+mkdir public/storage
+mkdir public/storage/product-images
+mkdir public/storage/backgrounds
+mkdir public/assets/ace
+git clone https://github.com/ajaxorg/ace.git public/assets/ace
+npm install
